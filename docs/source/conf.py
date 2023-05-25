12e80d1ebdb2bbb57223403432700c4087524880
Vibhor Aggarwal <vibhorag24@gmail.com>
2023-05-25 14:12:13 +0200

    Update conf.py
    updating the copyright to check

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2023, Vibhor'
+author = 'Vibhor'
 
 release = '0.1'
 version = '0.1.0'
